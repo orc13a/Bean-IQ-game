274d98b0fe8592d53cf77721988e5a4e6e5ef1af
Oliver Christensen <orc13a@live.dk>
2021-03-02 21:53:43 +0100

    changes

--- 2D/app.js
+++ 2D/app.js
@@ -40,10 +40,26 @@ function draw() {
 }
 
 function c() {
-    for (let i = 1; i < 5; i++) {
-        allBalls['blue'].push(new Ball((width / 2) + 25, ((height / 2) - 69) + (i * 28), 25, "#00bfff"));
-    }
-    for (let i = 1; i < 5; i++) {
-        allBalls['blue'].push(new Ball((width / 2) + 25 + 125, ((height / 2) - 69) + (i * 28), 25, "#00bfff"));
+    // allBalls['blue'].push(new Ball((width / 2) + 25, ((height / 2) - 69) + (i * 28), 25, "#00bfff"));
+    for (let i = 1; i < 20; i++) {
+        var ballX;
+        var ballY;
+        var newBall;
+        
+        // if (i < 5) {
+        //     ballX = (width / 2) + 25;
+        //     ballY = ((height / 2) - 62) + (i * 25.5);
+        //     newBall = new Ball(ballX, ballY, 25, "#00bfff");
+        // }
+
+        if (i > 2 && i < 10) {
+            ballX = ((width / 2) + (10 * (i * i)) + (0.2 * i));
+            ballY = (height / 2) + (25 * i);
+            newBall = new Ball(ballX, ballY, 25, "#00bfff");
+        }
+
+        if (newBall != undefined) {
+            allBalls['blue'].push(newBall);
+        }
     }
 }
